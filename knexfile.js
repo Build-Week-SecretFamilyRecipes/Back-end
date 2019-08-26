61f2a171df6a971bf9224eb2d04ecbed2fe09376
York Cook <yorkcook@Johnny-Castle.local>
2019-08-26 16:27:02 -0400

    WIP: first attempt to deploy to heroku

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,14 @@
 // Update with your config settings.
 
+const localPg = {
+  host: "localhost",
+  database: "users",
+  user: "York",
+  password: "baller"
+};
+
+const productionDbConnection = process.env.DATA_URL || localPg;
+
 module.exports = {
   development: {
     client: "sqlite3",
@@ -15,35 +24,34 @@ module.exports = {
     }
   },
 
-  staging: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: "knex_migrations"
-    }
-  },
+  // staging: {
+  //   client: "postgresql",
+  //   connection: {
+  //     database: "my_db",
+  //     user: "username",
+  //     password: "password"
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: "knex_migrations"
+  //   }
+  // },
 
   production: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
-    },
+    client: "pg",
+    connection: productionDbConnection,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: "knex_migrations"
+      directory: "./database/migrations"
+    },
+    seeds: {
+      directory: "./database/seeds"
     }
   }
 };
